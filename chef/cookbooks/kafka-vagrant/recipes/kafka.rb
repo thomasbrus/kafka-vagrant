cb761d5f4fdb2cb05ca33f7e6fe1ed09cecb30f1
Thomas Brus <thomas.brus@nedap.com>
2016-09-13 16:12:58 +0200

    Create kafka topics with each 3 partitions and replication factor of 3

--- chef/cookbooks/kafka-vagrant/recipes/kafka.rb
+++ chef/cookbooks/kafka-vagrant/recipes/kafka.rb
@@ -4,3 +4,31 @@ include_recipe 'kafka::default'
 service 'kafka' do
   action :start
 end
+
+execute 'kafka-topics.sh --create' do
+  command <<-COMMAND % node['kafka']['broker']['zookeeper.connect']
+    /opt/kafka/bin/kafka-topics.sh --create --topic elections --partitions 3 --replication 3 --zookeeper %s
+  COMMAND
+
+  # not_if <<-NOT_IF % node['kafka']['broker']['zookeeper.connect']
+  #   /opt/kafka/bin/kafka-topics.sh --list --zookeeper %s | grep elections
+  # NOT_IF
+end
+
+execute 'kafka-topics.sh --create' do
+  command <<-COMMAND % node['kafka']['broker']['zookeeper.connect']
+    /opt/kafka/bin/kafka-topics.sh --create --topic identity-access --partitions 3 --replication 3 --zookeeper %s
+  COMMAND
+end
+
+execute 'kafka-topics.sh --create' do
+  command <<-COMMAND % node['kafka']['broker']['zookeeper.connect']
+    /opt/kafka/bin/kafka-topics.sh --create --topic identities --partitions 3 --replication 3 --zookeeper %s
+  COMMAND
+end
+
+execute 'kafka-topics.sh --create' do
+  command <<-COMMAND % node['kafka']['broker']['zookeeper.connect']
+    /opt/kafka/bin/kafka-topics.sh --create --topic default --partitions 3 --replication 3 --zookeeper %s
+  COMMAND
+end
